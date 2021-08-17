d78d1da9d12ba95b888e640551a5d6373622fdad
teraitomoya <12a1070.sit.shonan@gmail.com>
2021-08-18 00:19:19 +0900

    “from、create完了“

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,14 +1,26 @@
 class PostsController < ApplicationController
+  before_action :set_tweet, only: [:show, :edit, :update, :destroy]
+
   def index
     @posts = Post.all
   end
   def new
     @post = Post.new
   end
+
   def create
-    Post.create(post_params)
-    redirect_to new_post_path
+    @post =.Post.new(post_params)
+    if params[:back]
+      render:new
+    else
+      if @post.save
+      redirect_to post_path,notice:"ポストを作成しました"
+      else
+      render:new,status:  unprocessable_entity
+      end
+    end
   end
+
 
   private
   def post_params
