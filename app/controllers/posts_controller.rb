ee23dda5ea62d6dfb43762c0557e3c9923420b19
teraitomoya <12a1070.sit.shonan@gmail.com>
2021-08-17 17:50:43 +0900

    “一覧表示機能編集“

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,17 +1,17 @@
 class PostsController < ApplicationController
   def index
+    @posts = Post.all
   end
   def new
-    Post.new
     @post = Post.new
   end
   def create
-    Post.create(params[:post])
+    Post.create(post_params)
     redirect_to new_post_path
   end
 
   private
   def post_params
-    params.require(:post).permit(:title, :content)
+    params.require(:post).permit(:content)
   end
 end
